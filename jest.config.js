670fb3c103e2b51ef34dde186a0f5af291e4a789
Mostafa Ismail Alanani <asmaylmr117@gmail.com>
2025-09-08 15:49:26 +0300

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
         ['@babel/preset-react', { runtime: 'automatic' }]
       ]
     }],
-  },
+  }, 
   collectCoverageFrom: [
     'src/**/*.{js,jsx}',
     '!src/index.js',
